d54c0c396223a69755e203361e9b2edb0ee9f56a
alex <strim0125@yandex.ru>
2022-02-10 15:13:28 +0300

    1.3

--- src/ru/iegs/p2_3/Main.java
+++ src/ru/iegs/p2_3/Main.java
@@ -1,10 +1,32 @@
 package ru.iegs.p2_3;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("---");
+        String[] words = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "kkk"};
+        System.out.println(Arrays.toString(words));
+
+        Map<String, Integer> hmap = new HashMap<>();
+        for (String x : words) {
+            hmap.put(x, hmap.getOrDefault(x, 0) + 1);
+        }
+        System.out.println(hmap);
+
+        Phone phBook = new Phone();
+
+        phBook.add("Иванов", "123456");
+        phBook.add("Петров", "1245234");
+        phBook.add("Сидоров", "7643245758");
+        phBook.add("Иванов", "378157578");
+        phBook.add("Петров", "64118235");
+
+        System.out.println("Тел" + phBook.get("Иванов"));
+        System.out.println("Тел" + phBook.get("Петров"));
+        System.out.println("Тел" + phBook.get("Сидоров"));
 
     }
 }
--- src/ru/iegs/p2_3/Phone.java
+++ src/ru/iegs/p2_3/Phone.java
@@ -1,10 +1,19 @@
 package ru.iegs.p2_3;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class Phone {
     private Map<String, Set<String>> phoneBook = new HashMap<>();
 
+    public void add(String name, String phone){
+        Set<String> tell = phoneBook.getOrDefault(name, new HashSet<>());
+        tell.add(phone);
+        phoneBook.put(name, tell);
+    }
+    public Set<String> get(String name){
+        return phoneBook.get(name);
+    }
 }
