1c61a70d3a70396bd389903bf19cc4d07e93cc7b
alex <strim0125@yandex.ru>
2022-02-10 13:56:33 +0300

    Initial commit

---
+++ src/ru/iegs/p2_3/Main.java
@@ -0,0 +1,10 @@
+package ru.iegs.p2_3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("---");
+
+    }
+}
---
+++ src/ru/iegs/p2_3/Phone.java
@@ -0,0 +1,10 @@
+package ru.iegs.p2_3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Phone {
+    private Map<String, Set<String>> phoneBook = new HashMap<>();
+
+}
